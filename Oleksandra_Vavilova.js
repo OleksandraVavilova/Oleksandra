7d0d32d8c22dac193bee946522fd3a24f412c638
OleksandraVavilova <166647691+OleksandraVavilova@users.noreply.github.com>
2024-05-03 21:14:42 +0200

    Add files via upload

--- Oleksandra_Vavilova.js
+++ Oleksandra_Vavilova.js
@@ -11,3 +11,43 @@ var marker = L.marker([40.55128137725361, -74.21251473397405]).addTo(map);
 
 // Add a popup to the marker
 marker.bindPopup("<b>Carvel</b><br>This is a Leaflet map.").openPopup();
+
+// Load the GeoJSON line file
+fetch('https://OleksandraVavilova.github.io/Oleksandra/Oleksandra_daytimeroute.geojson')
+    .then(response => response.json())
+    .then(geojson => {
+        // Customize the style of the line
+        var lineStyle = {
+            color: 'blue', // Change color as needed
+            weight: 5, // Change weight as needed
+            opacity: 0.7 // Change opacity as needed
+        };
+
+        // Add the GeoJSON line to the map
+        L.geoJSON(geojson, {
+            style: lineStyle
+        }).addTo(map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON file:', error);
+    });
+
+// Load the GeoJSON line file
+fetch('https://OleksandraVavilova.github.io/Oleksandra/Oleksandra_eveningroute.geojson')
+    .then(response => response.json())
+    .then(geojson => {
+        // Customize the style of the line
+        var lineStyle = {
+            color: 'red', // Change color as needed
+            weight: 5, // Change weight as needed
+            opacity: 0.7 // Change opacity as needed
+        };
+
+        // Add the GeoJSON line to the map
+        L.geoJSON(geojson, {
+            style: lineStyle
+        }).addTo(map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON file:', error);
+    });
