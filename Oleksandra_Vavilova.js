fa12ab76b8b789253813790b33ddbf85f4c523f4
OleksandraVavilova <166647691+OleksandraVavilova@users.noreply.github.com>
2024-05-04 14:27:10 +0200

    Add files via upload

--- Oleksandra_Vavilova.js
+++ Oleksandra_Vavilova.js
@@ -12,6 +12,87 @@ var marker = L.marker([40.55128137725361, -74.21251473397405]).addTo(map);
 // Add a popup to the marker
 marker.bindPopup("<b>Carvel</b><br>This is a Leaflet map.").openPopup();
 
+// Add a marker to the map for New York City
+var rockefellercentermarker = L.marker([40.75912164529136, -73.97860716497993]).addTo(map);
+
+// Add a popup to the marker
+rockefellercentermarker.bindPopup(`
+    <b>Rockefeller Center</b><br>A historic landmark featuring plazas and iconic attractions.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/luigis.png" style="width: 100px; height: auto;">
+`).openPopup();
+
+
+// Add a marker to the map for New York City
+var bryantparkmarker = L.marker([40.75358113759977, -73.98293393025328]).addTo(map);
+
+
+// Add a popup to the marker
+bryantparkmarker.bindPopup(`
+    <b>Bryant Park</b><br>A fantastic green space with cafes, perfect for relaxation and leisure in the heart of the city.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/pratt.jpg" style="width: 100px; height: auto;">
+`).openPopup();
+
+// Add a marker to the map for New York City
+var krispykrememarker = L.marker([40.75177572603904, -73.98612841772126]).addTo(map);
+
+
+// Add a popup to the marker
+krispykrememarker.bindPopup(`
+    <b>Krispy Kreme</b><br> Famous place for its delicious glazed doughnuts.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
+
+// Add a marker to the map for New York City
+var empirestatebuildingmarker = L.marker([40.74864125779539, -73.98563825262]).addTo(map);
+
+
+// Add a popup to the marker
+empirestatebuildingmarker.bindPopup(`
+    <b>Empire State Building</b><br>Iconic skyscraper with breathtaking views.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
+// Add a marker to the map for New York City
+var cityhallparkmarker = L.marker([40.712496509529146, -74.00652315490349]).addTo(map);
+
+
+// Add a popup to the marker
+cityhallparkmarker.bindPopup(`
+    <b>City Hall Park</b><br>City Hall Park has the oldest City Hall building in the US. The City Hall combines French Renaissance and English Neoclassical architectural styles. It is a beautiful and historical place<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
+// Add a marker to the map for New York City
+var brooklynbridgemarker = L.marker([40.706987872790904, -73.99781806664545]).addTo(map);
+
+
+// Add a popup to the marker
+brooklynbridgemarker.bindPopup(`
+    <b>Brooklyn Bridge Promenade</b><br>Brooklyn Bridge Promenade has good views of the city.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
+// Add a marker to the map for New York City
+var julianaspizzamarker = L.marker([40.702743353175784, -73.99345837026809]).addTo(map);
+
+
+// Add a popup to the marker
+julianaspizzamarker.bindPopup(`
+    <b>Juliana's Pizza</b><br>Great pizza place.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
+// Add a marker to the map for New York City
+var brooklynbridgeparkmarker = L.marker([40.70232734855437, -73.99587245724803]).addTo(map);
+
+
+// Add a popup to the marker
+brooklynbridgeparkmarker.bindPopup(`
+    <b>Brooklyn Bridge Park</b><br>Waterfront park with good views of the city and picnic areas.<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/psm.jpg"  style="width: 100px; height: auto;">
+`).openPopup();
+
 // Load the GeoJSON line file
 fetch('https://OleksandraVavilova.github.io/Oleksandra/Oleksandra_daytimeroute.geojson')
     .then(response => response.json())
